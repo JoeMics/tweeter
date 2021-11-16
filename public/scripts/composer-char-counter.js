d7e28fa9fa49aec908f323f2eebbc0ad125d9f56
Joseph Micla <josephmicla@gmail.com>
2021-11-16 14:25:53 -0800

    adds over-limit class to counter when input too large

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,16 @@
 $(document).ready(() => {
   $('#tweet-text').on('input', function(e) {
     const charLimit = 140;
-    const charCount = $(this).val().length; // the length of textarea text
+    const charCount = $(this).val().length;          // the length of textarea text
 
     const $counter = $(this).parent().find('.counter');
-    $counter.text(charLimit - charCount);
+    $counter.text(charLimit - charCount);           // shows user how many characters available
+
+    // the counter becomes red when over the character limit
+    if ($counter.val() < 0) {
+      return $counter.addClass('over-limit');
+    }
+
+    $counter.removeClass('over-limit');
   });
 });
